c0639d8557698ce528b52f1eadee14447276b709
himanshu <him.speedo@gmail.com>
2024-08-13 00:30:11 +0530

    enable disable banner

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,6 @@ import Banner from "./Banner";
 import { useBanner } from "./useBanner";
 import { ErrorAlert } from "./Dashboard";
 
-
 // Convert the date to the required format
 const formatDate = (dateString) => {
   if (!dateString) return "";
@@ -12,14 +11,16 @@ const formatDate = (dateString) => {
 };
 
 function Home() {
-  const { title, description, linkText, link, eventDate } = useBanner();
+  const { title, description, linkText, link, eventDate, isEnabled } =
+    useBanner();
   const targetDate = formatDate(eventDate);
   // const title = "TUF compitetion 2024";
   // const description = 'This is long description Join us in Denver from June 7 – 9 to see what’s coming next. This is long description Join us in Denver from June 7 – 9 to see what’s coming next. This is long description Join us in Denver from June 7 – 9 to see what’s coming next. This is long description Join us in Denver from June 7 – 9 to see what’s coming next. This is long description Join us in Denver from June 7 – 9 to see what’s coming next.';
   // const linkText = "Register now";
   // const link = "https://takeuforward.org/about-us";
 
-  const info = "NOTE - Sometimes Supabase pauses or shutdowns the database if there is no activity for a while. So, if you are not seeing the data, you can ping me on my contacts in my resume to resume the supabase service.";   
+  const info =
+    "NOTE - Sometimes Supabase pauses or shutdowns the database if there is no activity for a while. So, if you are not seeing the data, you can ping me on my contacts in my resume to resume the supabase service.";
 
   return (
     <>
@@ -28,14 +29,18 @@ function Home() {
           dashboard
         </div>
       </Link>
-      <Banner
-        title={title}
-        description={description}
-        linkText={linkText}
-        link={link}
-        targetDate={targetDate}
-      ></Banner>
-      
+      {isEnabled ? (
+        <Banner
+          title={title}
+          description={description}
+          linkText={linkText}
+          link={link}
+          targetDate={targetDate}
+        ></Banner>
+      ) : (
+        <ErrorAlert error="Banner is disabled, please enable it from the dashboard" />
+      )}
+
       <div className="my-10"></div>
 
       <ErrorAlert error={info} />
